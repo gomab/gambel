21e316c5c51f6e2278066b5e960b6c753a28342e
gomab <egoiomab@gamil.com>
2018-04-13 19:28:54 +0200

    Dynamic Content Display 2

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -19,6 +19,11 @@ class FrontEndController extends Controller
                 ->with('categories', Category::take(5)->get())
                 ->with('first_post', Post::orderBy('created_at', 'desc')->first())
                 ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
-                ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first());
+                ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+                ->with('culture', Category::find(7))
+                ->with('science', Category::find(8))
+                ->with('technology', Category::find(9))
+                ->with('settings', Setting::first());
+
     }
 }
